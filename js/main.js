1659d098119512d5e21b6d6d8399de5640227b32
Dmitry <48729817+dim-014@users.noreply.github.com>
2020-04-10 22:42:36 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const shoppingCartButton = document.querySelector('#cart-button');
+         modalWindow = document.querySelector('.modal');
+         closeButton = document.querySelector('.close');
+
+shoppingCartButton.addEventListener('click', function (event) {
+     modalWindow.classList.add('modal-active')
+});
+
+closeButton.addEventListener('click', function (ev) {
+     modalWindow.classList.remove('modal-active')
+});
